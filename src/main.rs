5eb14b084cfde2896e831e81719adfea3c72c668
Sebastian Schmidt <sebastian.schmidt@helsinki.fi>
2024-10-28 11:08:00 +0200

    More detailed logs.

--- src/main.rs
+++ src/main.rs
@@ -6,6 +6,7 @@ use lib_tsalign::a_star_aligner::{
 };
 use log::{info, LevelFilter};
 use serde::{Deserialize, Serialize};
+use serde_value::Value;
 use simplelog::{ColorChoice, TermLogger, TerminalMode};
 
 #[derive(Parser)]
@@ -13,7 +14,7 @@ struct Cli {
     #[clap(index = 1)]
     ground_truth_statistics: PathBuf,
 
-    #[clap(index = 1)]
+    #[clap(index = 2)]
     test_statistics: PathBuf,
 }
 
@@ -23,7 +24,7 @@ struct StatisticsFile {
     pub statistics: AlignmentResult<AlignmentType>,
 
     #[serde(flatten)]
-    pub parameters: HashMap<String, String>,
+    pub parameters: HashMap<String, Value>,
 }
 
 fn main() -> Result<(), String> {
@@ -43,14 +44,32 @@ fn main() -> Result<(), String> {
     );
     let ground_truth_statistics = read_to_string(&cli.ground_truth_statistics).unwrap();
     let ground_truth_statistics: StatisticsFile = toml::from_str(&ground_truth_statistics).unwrap();
-    let ground_truth_statistics = ground_truth_statistics.statistics;
+    let ground_truth_alignment = ground_truth_statistics.statistics.alignment;
+    let ground_truth_statistics = ground_truth_statistics.statistics.statistics;
 
     info!("Loading test statistics from {:?}", cli.test_statistics);
     let test_statistics = read_to_string(&cli.test_statistics).unwrap();
     let test_statistics: StatisticsFile = toml::from_str(&test_statistics).unwrap();
-    let test_statistics = test_statistics.statistics;
+    let test_alignment = test_statistics.statistics.alignment;
+    let test_statistics = test_statistics.statistics.statistics;
 
-    (ground_truth_statistics.alignment != test_statistics.alignment)
+    let mut error = false;
+
+    if ground_truth_alignment == test_alignment {
+        info!("The alignments are the same");
+    } else {
+        error = true;
+        info!("The alignments are NOT the same");
+    }
+
+    if ground_truth_statistics.cost == test_statistics.cost {
+        info!("The costs are the same");
+    } else {
+        error = true;
+        info!("The costs are NOT the same");
+    }
+
+    (!error)
         .then_some(())
-        .ok_or_else(|| "Alignment mismatch".to_string())
+        .ok_or_else(|| "see the log above".to_string())
 }
